6a7ce3c5268570ef1ec614758310a368fbeebc16
Esty Thomas <esty@umich.edu>
2020-01-24 16:14:12 -0500

    Add timing info

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::time::Instant;
+
 fn main() {
+    let now = Instant::now();
     let client = reqwest::Client::new();
     let request = client.get("https://quod.lib.umich.edu/cgi/o/oai/oai?verb=ListRecords&metadataPrefix=oai_dc&set=dlps%3Aherb00ic");
     let result = client.execute(request.build().unwrap()).unwrap().text().unwrap();
@@ -7,7 +10,7 @@ fn main() {
     let resumption_token = captures.get(1);
     match resumption_token {
         Some(token) => {
-            fetch_results(token.as_str().to_owned())
+            fetch_results(token.as_str().to_owned(), now)
         },
         None => {
             println!("done1!")
@@ -15,8 +18,8 @@ fn main() {
     };
 }
 
-fn fetch_results(resumption_token: String) {
-    println!("fetching for token: {}", resumption_token);
+fn fetch_results(resumption_token: String, now: Instant) {
+    println!("{} fetching for token: {}", now.elapsed().as_secs(), resumption_token);
     let result = reqwest::get(&format!("https://quod.lib.umich.edu/cgi/o/oai/oai?verb=ListRecords&resumptionToken={}", urlencoding::encode(&resumption_token))).unwrap().text().unwrap();
     //println!("{}", result);
     let re = regex::Regex::new(r"resumptionToken.*>(.*)</resumptionToken").unwrap();
@@ -27,7 +30,7 @@ fn fetch_results(resumption_token: String) {
             let token = c.get(1);
             match token {
                 Some(t) => {
-                    fetch_results(t.as_str().to_owned())
+                    fetch_results(t.as_str().to_owned(), now)
                 },
                 None => {
                     println!("done2!")
