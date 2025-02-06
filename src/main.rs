7e0769f08f8f96e10cae42f6d292d2e91e6086fc
Anthony Thomas <antmoth@umich.edu>
2025-02-06 16:08:54 -0500

    update dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::time::Instant;
+use reqwest::blocking::Client;
 
 fn main() {
     let now = Instant::now();
-    let client = reqwest::Client::new();
+    let client = Client::new();
     let request = client.get("https://<SERVER>/oai?verb=ListRecords&metadataPrefix=<PREFIX>&set=<SET>");
     let result = client.execute(request.build().unwrap()).unwrap().text().unwrap();
     let re = regex::Regex::new(r"resumptionToken.*>(.*)</resumptionToken").unwrap();
@@ -12,7 +13,7 @@ fn main() {
             let resumption_token = captures.get(1);
             match resumption_token {
                 Some(token) => {
-                    fetch_results(token.as_str().to_owned(), now)
+                    fetch_results(client, token.as_str().to_owned(), now)
                 },
                 None => {
                     println!("done1!")
@@ -25,9 +26,10 @@ fn main() {
     };
 }
 
-fn fetch_results(resumption_token: String, now: Instant) {
+fn fetch_results(client: Client, resumption_token: String, now: Instant) {
     println!("{} fetching for token: {}", now.elapsed().as_secs(), resumption_token);
-    let result = reqwest::get(&format!("https://<SERVER>/oai?verb=ListRecords&resumptionToken={}", urlencoding::encode(&resumption_token))).unwrap().text().unwrap();
+    let request = client.get(&format!("https://<SERVER>/oai?verb=ListRecords&resumptionToken={}", urlencoding::encode(&resumption_token)));
+    let result = client.execute(request.build().unwrap()).unwrap().text().unwrap();
     //println!("{}", result);
     let re = regex::Regex::new(r"resumptionToken.*>(.*)</resumptionToken").unwrap();
     let captures = re.captures(&result);
@@ -37,7 +39,7 @@ fn fetch_results(resumption_token: String, now: Instant) {
             let token = c.get(1);
             match token {
                 Some(t) => {
-                    fetch_results(t.as_str().to_owned(), now)
+                    fetch_results(client, t.as_str().to_owned(), now)
                 },
                 None => {
                     println!("done2!")
