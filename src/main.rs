7888853af2deb22979e32bad70cbfb44d9650464
Anthony Thomas <antmoth@umich.edu>
2025-04-04 10:48:59 -0400

    add argument handling for sets/records/formats, write option

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,96 @@
 use std::time::Instant;
+use std::fs;
 use reqwest::blocking::Client;
+use xee_xpath::{Documents, Queries, Query};
+use clap::error::ErrorKind;
+use clap::{Parser, Subcommand};
+use iri_string::types::{IriString};
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+#[command(name = "OAI Harvester")]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// OAI repository endpoint to harvest from
+    repository: IriString,
+
+    /// Verb to use
+    #[command(subcommand)]
+    verb: Verb,
+
+    #[arg(short, long)]
+    write: bool,
+}
+
+#[derive(Subcommand, Debug)]
+enum Verb {
+    /// List metadata formats available from this repository
+    ListMetadataFormats,
+    /// Get all records specified by metadataPrefix and, optionally, set
+    ListRecords { metadata_prefix: String, set: Option<String> },
+    /// Get the set structure of this repository
+    ListSets,
+}
 
 fn main() {
-    let now = Instant::now();
+    let args = Args::parse();
+    println!("args: {:?}", args);
+    // https://www.sifet.org/bollettino/index.php/bollettinosifet/oai
+    let repository = args.repository;
+    let write = args.write;
     let client = Client::new();
-    let request = client.get("https://<SERVER>/oai?verb=ListRecords&metadataPrefix=<PREFIX>&set=<SET>");
+    match &args.verb {
+        Verb::ListMetadataFormats => {
+            get_metadata_formats(client, repository, write);
+        },
+        Verb::ListRecords { metadata_prefix, set } => {
+            get_records(client, repository, metadata_prefix, set, write);
+        },
+        Verb::ListSets => {
+            get_sets(client, repository, write);
+        },
+    };
+}
+
+fn write_result(filepath: &str, result: &str) {
+    let path: PathBuf = PathBuf::from(filepath);
+    fs::write(path.as_path(), result);
+}
+
+fn get_metadata_formats(client: Client, repository: IriString, write: bool) {
+    let request_target = format!("{}?verb=ListMetadataFormats", repository.to_string());
+    let request = client.get(request_target);
     let result = client.execute(request.build().unwrap()).unwrap().text().unwrap();
+    if write {
+        write_result("formats.xml", &result);
+    }
+}
+
+fn get_sets(client: Client, repository: IriString, write: bool) {
+    let request_target = format!("{}?verb=ListSets", repository.to_string());
+    let request = client.get(request_target);
+    let result = client.execute(request.build().unwrap()).unwrap().text().unwrap();
+    if write {
+        write_result("sets.xml", &result);
+    }
+}
+fn get_records(client: Client, repository: IriString, prefix: &String, set: &Option<String>, write: bool) {
+    let now = Instant::now();
+    let request_base = format!("{}?verb=ListRecords", repository.to_string());
+    let request_target = format!("{}&metadataPrefix={}{}", request_base, prefix, match set {
+        Some(s) => format!("&set={}", s),
+        None => "".to_string(),
+    });
+    let request = client.get(request_target);
+    let result = client.execute(request.build().unwrap()).unwrap().text().unwrap();
+    let base_filename = format!("{}-{}", prefix, match set {
+        Some(s) => s,
+        None => "all",
+    });
+    if write {
+        let filename = format!("{}-0.xml", base_filename);
+        write_result(&filename, &result);
+    }
     let re = regex::Regex::new(r"resumptionToken.*>(.*)</resumptionToken").unwrap();
     let captures_opt = re.captures(&result);
     match captures_opt {
@@ -13,7 +98,7 @@ fn main() {
             let resumption_token = captures.get(1);
             match resumption_token {
                 Some(token) => {
-                    fetch_results(client, token.as_str().to_owned(), now)
+                    fetch_results(client, request_base, token.as_str().to_owned(), now, write, &base_filename)
                 },
                 None => {
                     println!("done1!")
@@ -26,11 +111,16 @@ fn main() {
     };
 }
 
-fn fetch_results(client: Client, resumption_token: String, now: Instant) {
-    println!("{} fetching for token: {}", now.elapsed().as_secs(), resumption_token);
-    let request = client.get(&format!("https://<SERVER>/oai?verb=ListRecords&resumptionToken={}", urlencoding::encode(&resumption_token)));
+fn fetch_results(client: Client, request_base: String, resumption_token: String, now: Instant, write: bool, base_filename: &str) {
+    let elapsed = now.elapsed().as_secs();
+    println!("{} fetching for token: {}", elapsed, resumption_token);
+    let request = client.get(&format!("{}&resumptionToken={}", request_base, urlencoding::encode(&resumption_token)));
     let result = client.execute(request.build().unwrap()).unwrap().text().unwrap();
     //println!("{}", result);
+    if write {
+        let filename = format!("{}-{}.xml", base_filename, elapsed.to_string());
+        write_result(&filename, &result);
+    }
     let re = regex::Regex::new(r"resumptionToken.*>(.*)</resumptionToken").unwrap();
     let captures = re.captures(&result);
     //println!("{:?}", captures);
@@ -39,7 +129,7 @@ fn fetch_results(client: Client, resumption_token: String, now: Instant) {
             let token = c.get(1);
             match token {
                 Some(t) => {
-                    fetch_results(client, t.as_str().to_owned(), now)
+                    fetch_results(client, request_base, t.as_str().to_owned(), now, write, base_filename)
                 },
                 None => {
                     println!("done2!")
