92ce5da72d819457897b4afb6c6c7203798b1897
Anthony Thomas <antmoth@umich.edu>
2025-04-07 13:26:18 -0400

    add 'until'; begin experimental xpath querying

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use std::time::Instant;
 use std::fs;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::path::PathBuf;
 use reqwest::blocking::Client;
-use xee_xpath::{Documents, Queries, Query};
-use clap::error::ErrorKind;
+use xee_xpath::{Documents, Itemable, Queries, Query};
+use xee_xpath::context::{StaticContextBuilder};
 use clap::{Parser, Subcommand};
 use iri_string::types::{IriString};
-use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
 #[command(name = "OAI Harvester")]
@@ -32,10 +35,15 @@ enum Verb {
         #[arg(short, long)]
         set: Option<String>,
         #[arg(short, long)]
-        from: Option<String>
+        from: Option<String>,
+        #[arg(short, long)]
+        until: Option<String>,
     },
     /// Get the set structure of this repository
     ListSets,
+    TestXpath {
+        infile: PathBuf,
+    },
 }
 
 fn main() {
@@ -54,21 +62,70 @@ fn main() {
             println!("Listing metadata formats available from {}", repository);
             get_metadata_formats(client, repository, write);
         },
-        Verb::ListRecords { metadata_prefix, set, from } => {
-            println!("Harvesting records from {} using prefix {} from {}{}", repository, metadata_prefix, match set {
+        Verb::ListRecords { metadata_prefix, set, from, until } => {
+            println!("Harvesting records from {} using prefix {} from {}{}{}", repository, metadata_prefix, match set {
                 Some(s) => format!("set {}", s),
                 None => "all sets".to_string(),
             }, match from {
                 Some(s) => format!(" starting from date {}", s),
                 None => "".to_string(),
+            }, match until {
+                Some(s) => format!(" until date {}", s),
+                None => "".to_string(),
             });
-            get_records(client, repository, metadata_prefix, set, from, write);
+            get_records(client, repository, metadata_prefix, set, from, until, write);
         },
         Verb::ListSets => {
             println!("Listing sets available from {}", repository);
             get_sets(client, repository, write);
         },
+        Verb::TestXpath { infile } => {
+            test_xpath(infile);
+        },
     };
+}
+
+fn test_xpath(infile: &PathBuf) -> Result<(), anyhow::Error> {
+    let mut reader: Box<dyn BufRead> = Box::new(BufReader::new(File::open(infile)?));
+
+    let mut input_xml = String::new();
+    reader.read_to_string(&mut input_xml)?;
+
+    let mut documents = Documents::new();
+    let doc = documents.add_string_without_uri(&input_xml)?;
+
+    let mut static_context_builder = StaticContextBuilder::default();
+    static_context_builder.default_element_namespace("http://www.openarchives.org/OAI/2.0/");
+
+    let queries = Queries::new(static_context_builder);
+    println!("{:?}", queries);
+
+    let xpath = "//resumptionToken";
+    let sequence_query = queries.sequence(xpath);
+    let sequence_query = match sequence_query {
+        Ok(sequence_query) => sequence_query,
+        Err(e) => {
+            println!("{:?}", e);
+            return Ok(());
+        }
+    };
+    let mut context_builder = sequence_query.dynamic_context_builder(&documents);
+    context_builder.context_item(doc.to_item(&documents)?);
+    let context = context_builder.build();
+
+    let sequence = sequence_query.execute_with_context(&mut documents, &context);
+    let sequence = match sequence {
+        Ok(sequence) => sequence,
+        Err(e) => {
+            println!("{:?}", e);
+            return Ok(());
+        }
+    };
+    println!(
+        "{}",
+        sequence.display_representation(documents.xot(), &context)
+    );
+    Ok(())
 }
 
 fn write_result(filepath: &str, result: &str) {
@@ -93,14 +150,17 @@ fn get_sets(client: Client, repository: IriString, write: bool) {
         write_result("sets.xml", &result);
     }
 }
-fn get_records(client: Client, repository: IriString, prefix: &String, set: &Option<String>, from: &Option<String>, write: bool) {
+fn get_records(client: Client, repository: IriString, prefix: &String, set: &Option<String>, from: &Option<String>, until: &Option<String>, write: bool) {
     let now = Instant::now();
     let request_base = format!("{}?verb=ListRecords", repository.to_string());
-    let request_target = format!("{}&metadataPrefix={}{}{}", request_base, prefix, match set {
+    let request_target = format!("{}&metadataPrefix={}{}{}{}", request_base, prefix, match set {
         Some(s) => format!("&set={}", s),
         None => "".to_string(),
     }, match from {
         Some(s) => format!("&from={}", s),
+        None => "".to_string(),
+    }, match until {
+        Some(s) => format!("&until={}", s),
         None => "".to_string(),
     });
     let request = client.get(request_target);
@@ -114,6 +174,8 @@ fn get_records(client: Client, repository: IriString, prefix: &String, set: &Opt
         let filename = format!("{}-0.xml", base_filename);
         write_result(&filename, &result);
     }
+
+
     let re = regex::Regex::new(r"resumptionToken.*>(.*)</resumptionToken").unwrap();
     let captures_opt = re.captures(&result);
     match captures_opt {
